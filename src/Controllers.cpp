6306e6454f6f1fa3f1aa62f1149bc49986885050
Brin Harper <brin.harper@team2342.org>
2015-11-09 23:28:16 -0500

    Created attachment/controller classes, added to constants doc

---
+++ src/Controllers.cpp
@@ -0,0 +1,20 @@
+/*
+ * Controllers.cpp
+ *
+ *  Created on: Nov 9, 2015
+ *      Author: Brin Harper
+ */
+
+#include "Controllers.h"
+
+Controllers::Controllers() :
+	m_stick(Port::joystickChannel)
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+Controllers::~Controllers() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/Loader.cpp
@@ -0,0 +1,17 @@
+/*
+ * Loader.cpp
+ *
+ *  Created on: Nov 9, 2015
+ *      Author: Brin Harper
+ */
+
+#include "Loader.h"
+
+Loader::Loader() {
+
+}
+
+Loader::~Loader() {
+	// TODO Auto-generated destructor stub
+}
+
